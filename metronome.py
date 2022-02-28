9be55f654bffb61c83b3c0493cc09adbcfd03b17
HassnHamada <hassneltokhy4@gmail.com>
2022-02-28 12:34:25 +0200

    renamed and organized some files

--- metronome.py
+++ metronome.py
@@ -1,57 +1,22 @@
-import speech_recognition as sr
+SECONDS_PER_MINUTE = 60
 
 
-def transcript_mic(mic: sr.Microphone, noise_filter_duration: int = 0, phrase_time_limit: int = 10) -> str:
+def metronome(input_string: str, time: float or int = 0) -> int:
     r"""
-    Extracte words from a voice stream
+    Return number of words in a string or the WPM if time is not zero
 
     Parameters
     ----------
-    mic : sr.Microphone
-        The microphone to work with
-    noise_filter_duration : int, default 0
-        The time used to calibrate the noise filtering
-    phrase_time_limit : int, default 10
-        How long to keep the microphone on
-
+    input_string : str
+        A string whom length is needed
+    time : float or int, default 0
+        The time window in seconds if the WPM is needed
     Returns
     -------
-    str
-        The extracte words
+    int
+        The input length
+    float 
+        WPM
     """
-    recognizer = sr.Recognizer()
-    with mic as source:
-        recognizer.adjust_for_ambient_noise(
-            source, duration=noise_filter_duration)
-        audio = recognizer.listen(source, phrase_time_limit=phrase_time_limit)
-    return recognizer.recognize_google(audio)
-
-
-def transcript_file(file: sr.AudioFile, noise_filter=False):
-    """
-    For test only
-    MUST NOT BE USED
-    """
-    assert __name__ == "__main__"
-    recognizer = sr.Recognizer()
-    with file as source:
-        if noise_filter:
-            recognizer.adjust_for_ambient_noise(source, duration=1)
-        audio = recognizer.record(source)
-    return recognizer.recognize_google(audio)
-
-
-def main():
-    """
-    For test only
-    MUST NOT BE USED
-    """
-    assert __name__ == "__main__"
-    # path = r"C:\Users\Hassn Hamada\Desktop\audio_files_harvard.wav"
-    # print(transcript_file(sr.AudioFile(path)))
-    mic = sr.Microphone()
-    print(transcript_mic(mic))
-
-
-if __name__ == "__main__":
-    main()
+    word_count = len(input_string.split())
+    return word_count if time == 0 else word_count / time * SECONDS_PER_MINUTE
