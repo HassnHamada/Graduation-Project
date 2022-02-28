4662c1822b58d2f99b2a9771ebbf5ee5298699f8
HassnHamada <hassneltokhy4@gmail.com>
2022-02-28 13:17:36 +0200

    added documentation for dictation

--- dictation.py
+++ dictation.py
@@ -2,5 +2,21 @@ from language_tool_python import LanguageTool
 
 
 def correct(inp_str: str, language: str = "en-UK") -> str:
+    r"""
+    Correct grammatical and spelling errors
+
+    Parameters
+    ----------
+    inp_str : str
+        A string to be corrected
+    language : str, default "en-UK"
+        The language to be used
+
+    Returns
+    -------
+    str
+        The corrected string
+    """
+
     tool = LanguageTool(language)
     return tool.correct(inp_str)
